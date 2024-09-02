1fad75b7f85b0e16ee57b3c8e0eddf574e4cdcf0
Minh Quang <nguyenminhquangnmqlamdong@gmail.com>
2024-09-02 18:17:34 +0700

    darkmode animation

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // this code is for switch mode
+    // which will save the previous mode to
+    // the local storage
+    let darkMode = localStorage.getItem("darkmode");
+    const themeSwitch = document.getElementById("theme-swtich");
+    
+    const enableDarkMode = () => {
+        document.body.classList.add("darkmode");
+        localStorage.setItem("darkmode", "active");
+    }
+
+    const disableDarkMode = () => { 
+        document.body.classList.remove("darkmode");
+        localStorage.setItem("darkmode", null);
+    }
+
+    if(darkMode === "active") enableDarkMode();
+    themeSwitch.addEventListener("click", () => {
+        darkMode = localStorage.getItem("darkmode");
+        darkMode !== "active" ? enableDarkMode() : disableDarkMode();
+    })
     const screen = document.querySelector(".screen");
     const buttons = document.querySelectorAll(".button");
     const erbutton =  document.querySelector(".er");
     let currentInput = "";
-
+    // this code is for displaying number 
+    // to the screen when buttons clicked
     function updateScreen(value){
         screen.textContent = value;
+        if (document.body.classList.contains("darkmode")) {
+            screen.style.color = "black"; // Light text for dark mode
+        }
     }
 
     buttons.forEach(button => {
@@ -36,4 +61,6 @@ document.addEventListener("DOMContentLoaded", function() {
         currentInput = "";
         updateScreen(currentInput);
     });
+
+    
 });
