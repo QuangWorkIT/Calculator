d262f83daab91d1c4af42326c3e4b3d01ce3c40d
Minh Quang <nguyenminhquangnmqlamdong@gmail.com>
2024-09-03 13:04:40 +0700

    new switch animation

--- script.js
+++ script.js
@@ -1,65 +1,69 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // this code is for switch mode
-    // which will save the previous mode to
-    // the local storage
-    let darkMode = localStorage.getItem("darkmode");
-    const themeSwitch = document.getElementById("theme-swtich");
-    
-    const enableDarkMode = () => {
-        document.documentElement.classList.add("darkmode");
-        localStorage.setItem("darkmode", "active");
-    }
+    document.addEventListener("DOMContentLoaded", function() {
+        // this code is for switch mode
+        // which will save the previous mode to
+        // the local storage
+        let darkMode = localStorage.getItem("darkmode");
+        const themeSwitch = document.getElementById("theme-switch");
 
-    const disableDarkMode = () => { 
-        document.documentElement.classList.remove("darkmode");
-        localStorage.setItem("darkmode", null);
-    }
-
-    if(darkMode === "active") enableDarkMode();
-    
-    themeSwitch.addEventListener("click", () => {
-        darkMode = localStorage.getItem("darkmode");
-        darkMode !== "active" ? enableDarkMode() : disableDarkMode();
-    })
-    const screen = document.querySelector(".screen");
-    const buttons = document.querySelectorAll(".button");
-    const erbutton =  document.querySelector(".er");
-    let currentInput = "";
-    // this code is for displaying number 
-    // to the screen when buttons clicked
-    function updateScreen(value){
-        screen.textContent = value;
-        if (document.documentElement.classList.contains("darkmode")) {
-            screen.style.color = "black"; // Light text for dark mode
+        const enableDarkMode = () => {
+            document.documentElement.classList.add("darkmode");
+            localStorage.setItem("darkmode", "active");
         }
-    }
 
-    buttons.forEach(button => {
-        button.addEventListener("click", function() {
-            const buttonText = this.textContent;
+        const disableDarkMode = () => { 
+            document.documentElement.classList.remove("darkmode");
+            localStorage.setItem("darkmode", null);
+        }
 
-            if(buttonText === "="){
-                try {
-                    currentInput = math.evaluate(currentInput).toString();
-                    if(currentInput === "Infinity" || currentInput === "-Infinity" ){
+        if(darkMode === "active") enableDarkMode(); 
+
+        themeSwitch.addEventListener("click", () => {
+            themeSwitch.classList.add("switch-click");
+            themeSwitch.addEventListener("animationend", () => {
+                themeSwitch.classList.remove("switch-click");
+            })
+            darkMode = localStorage.getItem("darkmode");
+            darkMode !== "active" ? enableDarkMode() : disableDarkMode();
+
+        })
+
+        const screen = document.querySelector(".screen");
+        const buttons = document.querySelectorAll(".button");
+        const erbutton =  document.querySelector(".er");
+        let currentInput = "";
+        
+        // this code is for displaying number 
+        // to the screen when buttons clicked
+        function updateScreen(value){
+            screen.textContent = value;
+        }
+
+        buttons.forEach(button => {
+            button.addEventListener("click", function() {
+                const buttonText = this.textContent;
+
+                if(buttonText === "="){
+                    try {
+                        currentInput = math.evaluate(currentInput).toString();
+                        if(currentInput === "Infinity" || currentInput === "-Infinity" ){
+                            console.log("Error");
+                            currentInput = "Hello World";
+                        }
+                        updateScreen(currentInput);
+                    } catch (error) {
                         console.log("Error");
-                        currentInput = "Hello World";
+                        currentInput = "";
+                        updateScreen(currentInput);
                     }
-                    updateScreen(currentInput);
-                } catch (error) {
-                    console.log("Error");
-                    currentInput = "";
+                }else{
+                    currentInput += buttonText;
                     updateScreen(currentInput);
                 }
-            }else{
-                currentInput += buttonText;
-                updateScreen(currentInput);
-            }
+            });
+        });
+
+        erbutton.addEventListener("click", function() {
+            currentInput = "";
+            updateScreen(currentInput);
         });
     });
-
-    erbutton.addEventListener("click", function() {
-        currentInput = "";
-        updateScreen(currentInput);
-    });
-});
